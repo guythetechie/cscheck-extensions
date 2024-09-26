66bbc379cc17a71765f8f0432d758086533c38c3
guythetechie <guythetechie@email.com>
2024-09-26 12:39:58 -0400

    Initial commit

---
+++ src/cscheck.extensions.tests/Generator.cs
@@ -0,0 +1,76 @@
+﻿using CsCheck;
+using FluentAssertions;
+using System.Collections.Frozen;
+using System.Collections.Immutable;
+using System.Linq;
+using Xunit;
+
+namespace cscheck.extensions.tests;
+
+
+public class GeneratorTests
+{
+    [Fact]
+    public void NonEmpty_ImmutableArray_is_not_empty()
+    {
+        var generator = Gen.Byte
+                           .ImmutableArrayOf()
+                           .NonEmpty();
+
+        generator.Sample(array => array.Should().NotBeEmpty());
+    }
+
+    [Fact]
+    public void NonEmpty_FrozenSet_is_not_empty()
+    {
+        var generator = Gen.Byte
+                           .FrozenSetOf()
+                           .NonEmpty();
+
+        generator.Sample(set => set.Should().NotBeEmpty());
+    }
+
+    [Fact]
+    public void SubImmutableArrayOf_returns_subset()
+    {
+        var generator = from source in Gen.String.List
+                        from subset in Generator.SubImmutableArrayOf(source)
+                        select (source, subset);
+
+        generator.Sample((source, subset) => subset.Should().BeSubsetOf(source));
+    }
+
+    [Fact]
+    public void SubFrozenSetOf_set_returns_subset()
+    {
+        var generator = from source in Gen.String.List
+                        from subset in Generator.SubFrozenSetOf(source.ToFrozenSet())
+                        select (source, subset);
+
+        generator.Sample((source, subset) => subset.Should().BeSubsetOf(source));
+    }
+
+    [Fact]
+    public void SubFrozenSetOf_collection_returns_subset()
+    {
+        var generator = from source in Gen.String.List
+                        from subset in Generator.SubFrozenSetOf(source)
+                        select (source, subset);
+
+        generator.Sample((source, subset) => subset.Should().BeSubsetOf(source));
+    }
+
+    [Fact]
+    public void OptionOf_returns_somes_or_nones()
+    {
+        var generator = Gen.Byte
+                           .OptionOf()
+                           .Array[1000];
+
+        generator.Sample(options =>
+        {
+            var somesPercentage = options.Count(option => option.IsSome) / (double)options.Length;
+            somesPercentage.Should().BeApproximately(0.8, 0.1);
+        });
+    }
+}
---
+++ src/cscheck.extensions/Generator.cs
@@ -0,0 +1,77 @@
+﻿using CsCheck;
+using LanguageExt;
+using System.Collections;
+using System;
+using System.Collections.Frozen;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace cscheck.extensions;
+
+public static class Generator
+{
+    public static Gen<ImmutableArray<T>> ImmutableArrayOf<T>(this Gen<T> gen) =>
+        from array in gen.Array
+        select array.ToImmutableArray();
+
+    public static Gen<FrozenSet<T>> FrozenSetOf<T>(this Gen<T> gen, IEqualityComparer<T>? comparer = default) =>
+        from array in gen.ImmutableArrayOf()
+        select array.ToFrozenSet(comparer);
+
+    public static Gen<ImmutableArray<T>> NonEmpty<T>(this Gen<ImmutableArray<T>> array) =>
+        from items in array
+        where items.Length > 0
+        select items;
+
+    public static Gen<FrozenSet<T>> NonEmpty<T>(this Gen<FrozenSet<T>> set) =>
+        from items in set
+        where items.Count > 0
+        select items;
+
+    public static Gen<ImmutableArray<T>> SubImmutableArrayOf<T>(ICollection<T> collection) =>
+        collection.Count is 0
+        ? Gen.Const(ImmutableArray<T>.Empty)
+        : from items in Gen.Shuffle(collection.ToArray(), collection.Count)
+          select items.ToImmutableArray();
+
+    public static Gen<FrozenSet<T>> SubFrozenSetOf<T>(FrozenSet<T> set) =>
+        SubFrozenSetOf(set, set.Comparer);
+
+    public static Gen<FrozenSet<T>> SubFrozenSetOf<T>(ICollection<T> collection, IEqualityComparer<T>? comparer = default) =>
+        collection.Count is 0
+        ? Gen.Const(FrozenSet<T>.Empty)
+        : from items in Gen.Shuffle(collection.ToArray(), collection.Count)
+          select items.ToFrozenSet(comparer);
+
+    public static Gen<Option<T>> OptionOf<T>(this Gen<T> gen) =>
+        Gen.Frequency((1, Gen.Const(Option<T>.None)),
+                      (4, gen.Select(Option<T>.Some)));
+
+    public static Gen<ImmutableArray<T2>> TraverseToImmutableArray<T1, T2>(ICollection<T1> collection, Func<T1, Gen<T2>> f) =>
+        collection.Select(f)
+                  .SequenceToImmutableArray();
+
+    /// <summary>
+    /// Converts a list of generators into a generator of immutable arrays.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="gens"></param>
+    /// <returns></returns>
+    public static Gen<ImmutableArray<T>> SequenceToImmutableArray<T>(this IEnumerable<Gen<T>> gens) =>
+        gens.Aggregate(Gen.Const(ImmutableArray<T>.Empty),
+                       (acc, gen) => from items in acc
+                                     from item in gen
+                                     select items.Add(item));
+
+    public static Gen<FrozenSet<T2>> TraverseToFrozenSet<T1, T2>(ICollection<T1> collection, Func<T1, Gen<T2>> f, IEqualityComparer<T2>? comparer = default) =>
+        collection.Select(f)
+                  .SequenceToFrozenSet(comparer);
+
+    /// <summary>
+    /// Converts a list of generators into a generator of frozen sets.
+    /// </summary>
+    public static Gen<FrozenSet<T>> SequenceToFrozenSet<T>(this IEnumerable<Gen<T>> gens, IEqualityComparer<T>? comparer = default) =>
+        from items in gens.SequenceToImmutableArray()
+        select items.ToFrozenSet(comparer);
+}
